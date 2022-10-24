85ecd4854c8eb01ac0bf784ac8b0f2773109c243
Zhohfut <413732041@qq.com>
2022-10-24 15:56:10 +0800

    add code plot3D

--- demo.py
+++ demo.py
@@ -1,11 +1,13 @@
 from tvtk.api import tvtk
+from tvtkfunc import ivtk_scene,event_loop
 
-# 创建一个长方体数据源，并且同时设置其长宽高
 s = tvtk.CubeSource(x_length=1.0, y_length=2.0, z_length=3.0)
-# 使用PolyDataMapper将数据转换为图形数据
 m = tvtk.PolyDataMapper(input_connection=s.output_port)
-# 创建一个Actor
 a = tvtk.Actor(mapper=m)
+# win = ivtk_scene(a)
+# win.scene.isometric_view()
+# event_loop()
+#
 # 创建一个Renderer，将Actor添加进去
 r = tvtk.Renderer(background=(0, 0, 0))
 r.add_actor(a)
