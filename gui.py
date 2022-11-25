35329d37c93afb663b595692e2b2e11fcbd4b926
Zhohfut <413732041@qq.com>
2022-11-25 08:34:45 +0800

    add code gui message

--- gui.py
+++ gui.py
@@ -18,6 +18,8 @@ import matplotlib.pyplot as plt
 def picker_callback(picker):
     print(picker.pick_position)
     print(picker.selection_point)
+    model.states += str(Message("点击位置：" + str(picker.pick_position)))
+    model.states += str(Message("选择结点：" + str(picker.selection_point)))
     x1, y1, z1 = picker.selection_point
     mlab.figure(model.plane.mayavi_scene)
     mlab.points3d(x1, y1, z1, color=(1, 0, 0), resolution=100)
@@ -51,33 +53,31 @@ class Layout(HasTraits):
         Action(name="地图", action="do_vega", enabled_when="fileDir"),
         Action(name="常规", action="do_terrain", enabled_when="fileDir"),
     ]
+    style_map = {
+        'gist_earth': "多彩",
+        'Blues': "雪地",
+        'summer': "草地",
+        'Vega20b': "地图",
+        'terrain': "常规",
+    }
 
     dataHandler = DataHandler()
 
     def do_process(self):
         pass
+        # nx.draw(self.g, ax=self.axes, with_labels=True, pos=self.pos)
+        # self.figure.canvas.show()
 
     def do_show(self):
-        g = nx.Graph()
-        g.add_edge('1', '2')
-        g.add_edge('2', '3')
-        g.add_edge('1', '4')
-        g.add_edge('2', '4')
+        self.g = nx.Graph()
+        self.g.add_edge('1', '2')
+        self.g.add_edge('2', '3')
+        self.g.add_edge('1', '4')
+        self.g.add_edge('2', '4')
 
-        fixed_position = {'1': [1, 1], '2': [1.5, 0.8], '3': [1.7, 2.8], '4': [0.6, 3.3]}
-        pos = nx.spring_layout(g, pos=fixed_position)
-
-        colors = []
-        for i in range(g.number_of_nodes()):
-            if i == 2:
-                colors.append('#ff0000')
-            else:
-                colors.append('#1f7814')
-
-        axes = self.figure.add_subplot(111)
-
-        # fig, ax = plt.subplots()
-        nx.draw(g, ax=axes, with_labels=True, pos=pos, node_color=colors)  # add colors
+        self.pos = nx.spring_layout(self.g)
+        self.axes = self.figure.add_subplot(111)
+        nx.draw(self.g, ax=self.axes, with_labels=True, pos=self.pos)
         self.figure.canvas.show()
 
     def _processBtn_fired(self):
@@ -129,7 +129,7 @@ class Layout(HasTraits):
         figure = mlab.figure(self.plane.mayavi_scene)
         picker = figure.on_mouse_pick(picker_callback, type='point', button='Left')
         picker.tolerance = 0.01
-        self.do_show()
+        # self.do_show()
         # figure = mlab.gcf()
         # figure.scene.disable_render = True
         # # 用mlab.points3d建立红色和白色小球的集合
@@ -145,7 +145,7 @@ class Layout(HasTraits):
         #                   z1[0] - 0.1, z1[0] + 0.1)
 
     # 定义监听函数、更新视图
-    @on_trait_change('fileDir, style')
+    @on_trait_change('fileDir')
     def load_plot(self):
         if not self.fileDir:
             return
@@ -158,14 +158,30 @@ class Layout(HasTraits):
             figure = mlab.figure(self.plane.mayavi_scene)
             mlab.clf()
             self.plane.mlab.imshow(self.dataHandler.dataSource, colormap=self.style)
-            figure.on_mouse_pick(picker_callback, type='cell', button='Left')
+            # figure.on_mouse_pick(picker_callback, type='cell', button='Left')
             figure = mlab.figure(self.scene.mayavi_scene)
             mlab.clf()
             self.scene.mlab.surf(self.dataHandler.dataSource, colormap=self.style, warp_scale=0.2)  # 可视化平面，指定颜色与放缩的最值
-            figure.on_mouse_pick(picker_callback, type='world', button='Left')
+            # figure.on_mouse_pick(picker_callback, type='world', button='Left')
             self.states += str(Message("加载成功！"))
         except:
             self.states += str(Error("文件格式错误！"))
+
+    # 切换风格
+    @on_trait_change('style')
+    def change_style(self):
+        if not self.fileDir:
+            return
+        try:
+            figure = mlab.figure(self.plane.mayavi_scene)
+            mlab.clf()
+            self.plane.mlab.imshow(self.dataHandler.dataSource, colormap=self.style)
+            figure = mlab.figure(self.scene.mayavi_scene)
+            mlab.clf()
+            self.scene.mlab.surf(self.dataHandler.dataSource, colormap=self.style, warp_scale=0.2)
+            self.states += str(Message("风格切换为："+self.style_map[self.style]))
+        except:
+            self.states += str(Error("风格转换失败！"))
 
     # 定义视图的布局
     _diagram = HSplit(
@@ -200,11 +216,11 @@ class Layout(HasTraits):
         ),
         title='地形分割与拓扑关系构建可视化系统',
         menubar=MenuBar(
-            Menu(fileProcess[0], name='文件'),
+            Menu(fileProcess[0], process, show, name='文件'),
             Menu(styles[0], styles[1], styles[2], styles[3], styles[4], name='风格'),
         ),
         # menubar=StandardMenuBar,
-        buttons=_buttons,
+        # buttons=_buttons,
         width=1800,
         height=1200,
         resizable=True
